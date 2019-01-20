a48d9d92f4b43ce52df1b78b6db8fed048f415bb
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2019-01-20 18:45:42 +0900

    ローディング

---
+++ src/pages/loading01/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <a class="button is-info" @click="showLoading">
+      <span class="icon is-small">
+        <i class="fas fa-bomb"></i>
+      </span>
+      <span>クルクル</span>
+    </a>
+  </div>
+</template>
+
+<script>
+import Loading from "@/common/components/loading/Loading";
+
+export default {
+  methods: {
+    async showLoading() {
+      Loading.show();
+      await this.timeout(3000);
+      Loading.dismiss();
+
+      this.$toast.open("くるくるおわり");
+    },
+    timeout(mills) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, mills);
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/loading01/index.js
@@ -0,0 +1,4 @@
+import App from './App.vue'
+import AppLaunther from '@/common/AppLaunther'
+
+AppLaunther(App)
