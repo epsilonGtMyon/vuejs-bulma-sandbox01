42a963a1b9ef9a52bd81508429d9a271ebfa1c60
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2019-02-04 22:27:10 +0900

    関数へ

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const pageRoot = path.resolve(__dirname, 'src/pages') + '/'
 
-const entries = (function () {
-
+const entries = (function() {
   //通常のページ用
-  const pageEntries = {};
+  const pageEntries = {}
   fs.readdirSync(pageRoot).forEach(pageFolder => {
     fs.readdirSync(`${pageRoot}/${pageFolder}`)
       .filter(fileName => fileName === 'index.js')
@@ -20,111 +19,118 @@ const entries = (function () {
   //--------------------------------------
   //追加
   const optionals = {}
-  optionals['global-style'] = path.resolve(__dirname, 'src/common/css/global-style.scss')
+  optionals['global-style'] = path.resolve(
+    __dirname,
+    'src/common/css/global-style.scss'
+  )
 
-  const es = Object.assign({}, pageEntries, optionals);
-  return es;
-})();
+  const es = Object.assign({}, pageEntries, optionals)
+  return es
+})()
 
-module.exports = {
-  entry: entries,
-  output: {
-    filename: '[name].js',
-    chunkFilename: '[name].js',
-    path: path.resolve(__dirname, 'dist/compiled')
-  },
+module.exports = (env, argv) => {
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [{ loader: 'babel-loader' }]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: 'css-loader' }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        oneOf: [
-          {
-            resourceQuery: /^\?vue/,
-            use: [
-              { loader: 'vue-style-loader' },
-              { loader: 'css-loader' },
-              { loader: 'sass-loader' }
-            ]
-          },
-          {
-            use: [
-              { loader: MiniCssExtractPlugin.loader },
-              { loader: 'css-loader' },
-              { loader: 'sass-loader' }
-            ]
-          }
-        ]
-      },
-      {
-        test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          context: ''
-        }
+  const setting = {
+    entry: entries,
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+      path: path.resolve(__dirname, 'dist/compiled')
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
       }
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    })
-  ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          test: /node_modules.*\.js/,
-          name: 'vendors',
-          chunks: 'all',
-          priority: 1,
-          enforce: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
         },
-        appCommon: {
-          test: /src\\common.*\.js/,
-          name: 'app-common',
-          chunks: 'all',
-          priority: 0,
-          enforce: true
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [{ loader: 'babel-loader' }]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            { loader: 'css-loader' }
+          ]
+        },
+        {
+          test: /\.scss$/,
+          oneOf: [
+            {
+              resourceQuery: /^\?vue/,
+              use: [
+                { loader: 'vue-style-loader' },
+                { loader: 'css-loader' },
+                { loader: 'sass-loader' }
+              ]
+            },
+            {
+              use: [
+                { loader: MiniCssExtractPlugin.loader },
+                { loader: 'css-loader' },
+                { loader: 'sass-loader' }
+              ]
+            }
+          ]
+        },
+        {
+          test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            context: ''
+          }
+        }
+      ]
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      })
+    ],
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendors: {
+            test: /node_modules.*\.js/,
+            name: 'vendors',
+            chunks: 'all',
+            priority: 1,
+            enforce: true
+          },
+          appCommon: {
+            test: /src\\common.*\.js/,
+            name: 'app-common',
+            chunks: 'all',
+            priority: 0,
+            enforce: true
+          }
         }
       }
-    }
-  },
+    },
 
-  devServer: {
-    contentBase: __dirname,
-    compress: true,
-    port: 9000,
-    publicPath: '/dist/compiled',
-    open: 'chrome',
-    openPage: 'page/'
-  },
-  devtool: 'source-map '
+    devServer: {
+      contentBase: __dirname,
+      compress: true,
+      port: 9000,
+      publicPath: '/dist/compiled',
+      open: 'chrome',
+      openPage: 'page/'
+    },
+    devtool: 'source-map '
+  }
 
+  return setting
 }
